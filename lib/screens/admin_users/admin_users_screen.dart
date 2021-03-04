e41f850853e530acb1c4432b7a32853707a00641
Hayber Gabriel <haybergabrielr@gmail.com>
2021-03-03 22:07:54 -0300

    Variável endereco excluída (Já q n estamos mais usando ela, antes ela era usada na tela de cadastro, mas lá n pede mais endereço) e adicionado o botão do telefone lá na admin_users_screen.dart

--- lib/screens/admin_users/admin_users_screen.dart
+++ lib/screens/admin_users/admin_users_screen.dart
@@ -1,13 +1,21 @@
+import 'dart:async';
 import 'package:alphabet_list_scroll_view/alphabet_list_scroll_view.dart';
 import 'package:flutter/material.dart';
-import 'package:lojavirtual/common/bottom_nav_bar.dart';
 import 'package:lojavirtual/common/custom_drawer/custom_drawer.dart';
 import 'package:lojavirtual/models/admin_orders_manager.dart';
 import 'package:lojavirtual/models/admin_users_manager.dart';
 import 'package:lojavirtual/models/page_manager.dart';
+import 'package:lojavirtual/models/user.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AdminUsersScreen extends StatelessWidget {
+  final User user = User();
+
+  Future<void> openPhone(String x) async {
+    await launch('tel:${x}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,42 +25,43 @@ class AdminUsersScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: Consumer<AdminUsersManager>(
-        builder: (_, adminUsersManager, __){
+        builder: (_, adminUsersManager, __) {
           return AlphabetListScrollView(
-            itemBuilder: (_, index){
+            itemBuilder: (_, index) {
               return ListTile(
+                leading: IconButton(icon: Icon(Icons.phone),
+                  iconSize: 28,
+                  color: Colors.black,
+                  onPressed: () {openPhone(adminUsersManager.users[index].telefone);},),
                 title: Text(
                   adminUsersManager.users[index].name,
                   style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      color: Colors.black
-                  ),
+                      fontWeight: FontWeight.w800, color: Colors.black),
                 ),
                 subtitle: Text(
-                  '${adminUsersManager.users[index].email}\n${adminUsersManager.users[index].telefone}',
+                  '${adminUsersManager.users[index].email}}',
                   style: TextStyle(
                     color: Colors.black,
                   ),
                 ),
-                onTap: (){
-                  context.read<AdminOrdersManager>().setUserFilter(
-                    adminUsersManager.users[index]
-                  );
-                  context.read<PageManager>().setPage(7);
+                onLongPress: () {
+                  openPhone(adminUsersManager.users[index].telefone);
+                },
+                onTap: () {
+                  context
+                      .read<AdminOrdersManager>()
+                      .setUserFilter(adminUsersManager.users[index]);
+                  context.read<PageManager>().setPage(6);
                 },
               );
             },
-            highlightTextStyle: TextStyle(
-              color: Colors.black,
-              fontSize: 20
-            ),
+            highlightTextStyle: TextStyle(color: Colors.black, fontSize: 20),
             indexedHeight: (index) => 80,
             strList: adminUsersManager.names,
             showPreview: true,
           );
         },
       ),
-
     );
   }
 }
